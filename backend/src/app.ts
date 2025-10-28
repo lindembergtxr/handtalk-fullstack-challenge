69122f6a0dd1a34e1986fd0c021226283500c32c
Lindemberg Teixeira <lindemberglta@icloud.com>
2025-10-28 20:23:58 -0300

    feat(backend): configure cors on backend
    the cors is configure to accept all request methods and work with only the localhost of the frontend declared on the .env

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,22 @@
 import express from 'express'
+import cors from 'cors'
 import { router } from './routes/index.js'
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') })
+
+const corsOptions = {
+    origin: `http://localhost:${process.env.FRONTEND_PORT}`,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true,
+}
 
 export const createServer = () => {
     const app = express()
+
+    app.use(cors(corsOptions))
 
     app.use(express.json())
 
