d411d18baaea5ab4ee90b4d73189865a4f92d0ca
Lindemberg Teixeira <lindemberglta@icloud.com>
2025-10-28 18:08:48 -0300

    feat(frontend): add and configure tailwindcss

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), tailwindcss()],
     test: {
         environment: 'jsdom',
         globals: true,
