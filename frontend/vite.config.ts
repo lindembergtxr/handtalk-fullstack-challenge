70dc239a28df522171a026a707fe0462d0269c98
Lindemberg Teixeira <lindemberglta@icloud.com>
2025-10-28 21:10:55 -0300

    chore(frontend): adjust configuration and add axios
    - add axios
    - configure port to use env var
    - remove unnecessary tsconfig files and configure vue typing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,19 @@
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import { loadEnv } from 'vite'
 
-export default defineConfig({
-    plugins: [vue(), tailwindcss()],
-    test: {
-        environment: 'jsdom',
-        globals: true,
-    },
-})
+export default ({ mode }: { mode: any }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+
+    return defineConfig({
+        plugins: [vue(), tailwindcss()],
+        server: {
+            port: parseInt(env.VITE_APP_PORT, 10) || 5173,
+        },
+        test: {
+            environment: 'jsdom',
+            globals: true,
+        },
+    })
+}
