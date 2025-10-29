9b4bf558d8c922599ef1b207ef815e801ad9954b
Lindemberg Teixeira <lindemberglta@icloud.com>
2025-10-28 23:23:33 -0300

    feat(frontend): better handle the backend response

--- frontend/src/services/analyzeUrl.ts
+++ frontend/src/services/analyzeUrl.ts
@@ -1,6 +1,22 @@
+import axios, { AxiosError } from 'axios'
 import { backendInstance } from './api'
 
 export const analyzeUrl = async (url: string) => {
-    const response = await backendInstance.post('/api/analyze', { url })
-    return response
+    try {
+        const response = await backendInstance.post('/api/analyze', { url })
+
+        return { success: true, data: response.data }
+    } catch (err) {
+        let errorData: any = null
+
+        if (axios.isAxiosError(err)) {
+            const axiosErr = err as AxiosError
+            errorData = {
+                status: axiosErr.response?.status,
+                message: axiosErr.response?.data || axiosErr.message,
+            }
+        } else errorData = { message: (err as Error).message || 'Unknown error' }
+
+        return { success: false, error: errorData }
+    }
 }
